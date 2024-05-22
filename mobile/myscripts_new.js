7b5182cca7c2df74768a4db5a7c8eee35362792c
Brad Nielsen <brad_nielsen29@hotmail.com>
2024-05-23 09:42:26 +1000

    commit

--- mobile/myscripts_new.js
+++ mobile/myscripts_new.js
@@ -1,4 +1,9 @@
 // This is a JavaScript file
+//GLOBAL VARIABLES
+var userid = null;
+var username = null;
+var permission = null;
+
 
 function login()
 {
@@ -6,15 +11,40 @@ function login()
 
     email = document.getElementById('email').value;
     password = document.getElementById('password').value;
+
+    //validate input
+    if (email == "" || password == "")
+    {
+        return;
+    }
+    if (!email.includes('@')) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
+
     var formobject = new FormData(); 
     formobject.append("email", email);
     formobject.append("password", password);
     new_ajax_helper('https://python-yellow-bear-bradnielsen702.codeanyapp.com/mobilelogin', receivelogin, formobject,'POST');
 }
 
-
+//JSON is turned into an OBJECT when it comes
 function receivelogin(response)
 {
-    alert(response.status);
+    if (response.status == 'success')
+    {
+        alert("Welcome " + response.name);
+        username = response.name;
+        userid = response.userid;
+        permission = response.permission;
+
+        $.mobile.changePage( "#menupage", { transition: "flip" });
+
+    } else
+    {
+        alert(response.message);
+    }
 }
+
 
