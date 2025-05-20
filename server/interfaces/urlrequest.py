3387992cb5c2ea9bf3b0a6ccff9689dfe16c4744
Brad Nielsen <brad_nielsen29@hotmail.com>
2025-05-20 14:03:03 +1000

    commit

---
+++ server/interfaces/urlrequest.py
@@ -0,0 +1,15 @@
+import urllib.parse
+import urllib.request
+import time
+import torch
+
+#---EXTERNAL URL REQUEST LIBRARY---------------------------------------#
+def sendurlrequest(url, dictofvalues):
+    responsedata = None
+    data = urllib.parse.urlencode(dictofvalues)
+    data = data.encode('ascii') # data should be bytes
+    req = urllib.request.Request(url, data)
+    #if data is returned
+    with urllib.request.urlopen(req) as response:
+        responsedata = response.read().decode('utf-8')
+    return responsedata
