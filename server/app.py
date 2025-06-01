b02fb43d476a2b2facafa19f47a12a45adccfe77
Brad Nielsen <38126504+bradnielsen2981@users.noreply.github.com>
2025-06-02 09:25:12 +1000

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,71 +1,115 @@
-from flask import *
-import sys, os, logging, datetime
-from interfaces.databaseinterface import Database
-from interfaces.hashing import *
-from interfaces.helpers import *
-from flask_cors import CORS, cross_origin
+#---PYTHON LIBRARIES FOR IMPORT--------------------------------------
+import uuid, sys, logging, math, time, os, re
+from flask import Flask, render_template, session, request, redirect, url_for, flash, jsonify, send_from_directory
 from werkzeug.utils import secure_filename
-import requests
-import json
-
-#---CONFIGURE APP---------------------------------------------------
-app = Flask(__name__)
-cors = CORS(app)
-logging.basicConfig(filename='logs/flask.log', level=logging.INFO)
-sys.tracebacklimit = 10
-
-# Configure the upload folder and allowed file extensions
-UPLOAD_FOLDER = 'profilephotos'
-QRCODE_FOLDER = 'qrcodes'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['QRCODE_FOLDER'] = QRCODE_FOLDER
-app.config['SECRET_KEY'] = "Type in secret line of text"
+from databaseinterface import Database
+from datetime import datetime
+import requests #needed for an external API
+import helpers
+from flask_cors import CORS, cross_origin #----------NEEDS TO BE INSTALLED to allow cross origin pip install flask-cors
 
 # Function to check the file extension
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-DATABASE = Database("database/test.db", app.logger)
+#---CONFIGURE APP---------------------------------------------------#
+sys.tracebacklimit = 10 #Level of python traceback - useful for reducing error text
+app = Flask(__name__) #Creates the Flask Server Object
+app.config['SECRET_KEY'] = 'some random string' #used for session cookies
+CORS(app) #enables cross domain scripting protection - necessary when communicating with the app
+UPLOAD_FOLDER = 'photos'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = "Type in secret line of text"
+app.logger.setLevel(logging.INFO)
 
-#use @cross_origin() after @app.route to allow external access 
-#---VIEW FUNCTIONS----------------------------------------------------
-@app.route('/backdoor')
-def backdoor():
-    userdetails = DATABASE.ViewQuery("SELECT * FROM users")
-    return jsonify(results)
+DATABASE = Database('mysite/mydatabase.db', app.logger)
 
-@app.route('/logout')
-def logout():
-    app.logger.info("Log out")
-    session.clear()
-    return redirect('./')
+@app.route('/')
+def home():
+    app.logger.info("display users")
+    userdetails = "Welcome"
+    #userdetails = DATABASE.ViewQuery("SELECT * FROM users")
+    return jsonify(userdetails)
 
-#admin page
-@app.route('/admin', methods=["GET","POST"])
-def admin():
-
-    if 'permission' not in session:
-        return redirect("./")
-    else:
-        if session['permission'] != 'admin':
-            return redirect("./")
-
-    results = DATABASE.ViewQuery("SELECT * FROM users")
+@app.route('/login', methods=['GET','POST']) #default password is 'password'
+@cross_origin()
+def login():
+    app.logger.info("Login")
+    data = { "success":False, "message":"Login unsuccessful"}
 
     if request.method == "POST":
-        selectedusers = request.form.getlist("selectedusers")
-        for userid in selectedusers:
-            if int(userid) != 1:
-                DATABASE.ModifyQuery("DELETE FROM users WHERE userid = ?", (userid,))
-        return redirect("./admin")
+        email = request.form['email']
+        password = request.form['password']
 
-    app.logger.info("Admin")
-    return render_template("admin.html", results=results)
+        users = DATABASE.ViewQuery("SELECT * FROM users WHERE email = ?", (email,))
+        if users:
+            user = users[0] #only get one user - hopefully
+            if helpers.check_password(user[password], password):
 
-#return a profile photo
-@app.route('/profilephotos/<filename>')
+                app.logger.info("SUCCESSFUL LOGIN")
+
+                data['success'] = True
+                data['message'] = "Login successful"
+                data['userid'] = user['userid']
+                data['username'] = user['firstname'] + " " + user['lastname']
+                data['permission'] = user['permission']
+
+                #create a token using hashing
+                #save the token beside the userid in the database
+                #send the token to the mobile app data['token'] = ?
+
+
+    return jsonify(data) #returns the data to the app in JSON
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    if request.method == "POST":
+        token = request.form.get('token')
+        userid = request.form.get('userid')
+        #search for user with token in database and delete token
+    data = { "success":True, "message":"User token is cleared" }
+    return jsonify(data)
+
+@app.route('/chartsearch', methods=["GET","POST"])
+@cross_origin()
+def chartsearch():
+
+    app.logger.info("Search for a star chart")
+
+    current_date = datetime.today().strftime('%Y-%m-%d')
+    latitude = -27.4698
+    longitude = 153.0251
+
+    url = "https://astronomy.p.rapidapi.com/api/v2/studio/star-chart"
+    payload = {
+    	"observer": {
+    		"date": current_date,
+    		"latitude": latitude,
+    		"longitude": longitude
+    	},
+    	"style": "default",
+            "view": {
+                "type": "constellation",
+                "parameters": {
+                    "constellation": "sco"  # Orion, for example; can also use 'all' or others
+                }
+            }
+    }
+    headers = {
+    	"x-rapidapi-key": "cb92ceb2dbmsh28774b29f8647f1p1af226jsndde6d5bc1c30",
+    	"x-rapidapi-host": "astronomy.p.rapidapi.com",
+    	"Content-Type": "application/json"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+
+    return response.json()
+
+#app.route to post a photo
+
+#return photos from the server folder
+@app.route('/photos/<filename>')
 def serve_profilephoto(filename):
     if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)): # Ensure the file exists
         return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
@@ -73,62 +117,7 @@ def serve_profilephoto(filename):
         abort(404) # If the file does not exist, return a 404 error
     return
 
-# Exit the web server
-@app.route('/exit', methods=['GET','POST'])
-def exit():
-    app.logger.info("Exiting")
-    func = request.environ.get('werkzeug.server.shutdown')
-    func()
-    return jsonify({'message':'Exiting'})
-
-@app.route('/apisearch', methods=["GET","POST"])
-@cross_origin()
-def apisearch():
-    data = {}
-    request.method = 'POST'
-    if request.method == 'POST':
-        songtitle = 'Lucy in the sky with diamonds'
-        songartist = 'Beatles'
-        query = songtitle + " " + songartist
-        url = "https://spotify23.p.rapidapi.com/search/"
-        querystring = {"q":query,"type":"multi","offset":"0","limit":"5","numberOfTopResults":"5"}
-        headers = {
-            "X-RapidAPI-Key": "63a6d49d67mshf59bc1bb51752acp169684jsnd91195bff2b6",
-            "X-RapidAPI-Host": "spotify23.p.rapidapi.com"
-        }
-        response = requests.get(url, headers=headers, params=querystring)
-        dictionary = response.json()
-        data = dictionary['albums']['items']
-    return jsonify(data)
-
-#login page
-@app.route('/mobilelogin', methods=["GET","POST"])
-@cross_origin()
-def mobilelogin():
-    app.logger.info("Mobile Login")
-    data = {}
-    if request.method == "POST":
-        email = request.form['email']
-        password = request.form['password']
-        results = DATABASE.ViewQuery("SELECT * FROM users WHERE email = ?", (email,))
-        if results:
-            userdetails = results[0] #row in the user table (Python Dictionary)
-            if check_password(userdetails['password'], password):
-                #create a token using simple hash function
-                data['userid'] = userdetails['userid']
-                data['permission'] = userdetails['permission']
-                data['name'] = userdetails['firstname'] + " " + userdetails['lastname']
-                data['status'] = 'success'
-                data['message'] = 'Login successful'
-            else: 
-                data['status'] = 'failure'
-                data['message'] = 'Password incorrect!'
-        else:
-            data['status'] = 'failure'
-            data['message'] = 'Email does not match existing users'
-    return jsonify(data)
-
-
 #main method called web server application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True) #runs a local server
+    app.run()
+
